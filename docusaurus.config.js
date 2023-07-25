e9515755d274464dad253e00fa241efd795de810
Mahot Descelliers <mahot.descelliers@withings.com>
2023-07-25 14:17:28 +0200

    Use Google Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-9QNKTPSF5J',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
